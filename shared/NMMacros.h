22d6d4b2b73d799654030bf9d62027908210b7f9
Alexander Herzig <herziga@landcareresearch.co.nz>
2012-12-12 17:30:52 +1300

    enh: added more convenience macros for implementing itk::ProcessObject wrappers

--- shared/NMMacros.h
+++ shared/NMMacros.h
@@ -54,22 +54,27 @@ signals: \
  *
  */
 
+/** ==============================================================
+ * 					getOutput
+ *  ==============================================================
+ */
+
 #define callGetOutput( inputType, outputType, wrapName ) \
 { \
-	if (this->mOutputNumDimensions == 1)		\
+	if (this->mInputNumDimensions == 1)		\
 	{		\
 		img = wrapName< inputType, outputType, 1>::getOutput(this->mOtbProcess, \
-				this->mOutputNumBands);	\
+				this->mInputNumBands);	\
 	}		\
-	else if (this->mOutputNumDimensions == 2) \
+	else if (this->mInputNumDimensions == 2) \
 	{ \
 		img = wrapName< inputType, outputType, 2 >::getOutput(this->mOtbProcess, \
-				this->mOutputNumBands);	\
+				this->mInputNumBands);	\
 	} \
-	else if (this->mOutputNumDimensions == 3) \
+	else if (this->mInputNumDimensions == 3) \
 	{ \
 		img = wrapName< inputType, outputType, 3 >::getOutput(this->mOtbProcess, \
-				this->mOutputNumBands);	\
+				this->mInputNumBands);	\
 	}\
 }
 
@@ -92,6 +97,29 @@ signals: \
 	}\
 }
 
+#define callInputTypeGetOutput( inputType, wrapName ) \
+{ \
+	if (this->mInputNumDimensions == 1)		\
+	{		\
+		img = wrapName< inputType, 1>::getOutput(this->mOtbProcess, \
+				this->mInputNumBands);	\
+	}		\
+	else if (this->mInputNumDimensions == 2) \
+	{ \
+		img = wrapName< inputType, 2 >::getOutput(this->mOtbProcess, \
+				this->mInputNumBands);	\
+	} \
+	else if (this->mInputNumDimensions == 3) \
+	{ \
+		img = wrapName< inputType, 3 >::getOutput(this->mOtbProcess, \
+				this->mInputNumBands);	\
+	}\
+}
+
+/** ==============================================================
+ * 					createInstance
+ *  ==============================================================
+ */
 #define callCreator( inputType, outputType, wrapName ) \
 { \
 	if (this->mInputNumDimensions == 1)		\
@@ -111,7 +139,7 @@ signals: \
 	}\
 }
 
-#define callSingleTypeCreator( inputType, wrapName ) \
+#define callInputTypeCreator( inputType, wrapName ) \
 { \
 	if (this->mInputNumDimensions == 1)		\
 	{		\
@@ -130,6 +158,29 @@ signals: \
 	}\
 }
 
+#define callOutputTypeCreator( outputType, wrapName ) \
+{ \
+	if (this->mOutputNumDimensions == 1)		\
+	{		\
+		wrapName< outputType, 1>::createInstance(this->mOtbProcess, \
+				this->mOutputNumBands);	\
+	}		\
+	else if (this->mOutputNumDimensions == 2) \
+	{ \
+		wrapName< outputType, 2 >::createInstance(this->mOtbProcess, \
+				this->mOutputNumBands);	 \
+	} \
+	else if (this->mOutputNumDimensions == 3) \
+	{ \
+		wrapName< outputType, 3 >::createInstance(this->mOtbProcess, \
+				this->mOutputNumBands);	 \
+	}\
+}
+
+/** ==============================================================
+ * 					setNthInput
+ *  ==============================================================
+ */
 #define callSetInput( inputType, outputType, wrapName ) \
 { \
 	if (this->mInputNumDimensions == 1) \
@@ -149,10 +200,100 @@ signals: \
 	}\
 }
 
+#define callInputTypeSetInput( imgType, wrapName ) \
+{ \
+	if (this->mInputNumDimensions == 1) \
+	{ \
+		wrapName< imgType, 1 >::setNthInput( \
+				this->mOtbProcess, this->mInputNumBands, numInput, img); \
+	} \
+	else if (this->mInputNumDimensions == 2) \
+	{ \
+		wrapName< imgType, 2 >::setNthInput( \
+				this->mOtbProcess, this->mInputNumBands, numInput, img); \
+	} \
+	else if (this->mInputNumDimensions == 3) \
+	{ \
+		wrapName< imgType, 3 >::setNthInput( \
+				this->mOtbProcess, this->mInputNumBands, numInput, img); \
+	}\
+}
+
+#define callOutputTypeSetInput( imgType, wrapName ) \
+{ \
+	if (this->mOutputNumDimensions == 1) \
+	{ \
+		wrapName< imgType, 1 >::setNthInput( \
+				this->mOtbProcess, this->mInputNumBands, numInput, img); \
+	} \
+	else if (this->mOutputNumDimensions == 2) \
+	{ \
+		wrapName< imgType, 2 >::setNthInput( \
+				this->mOtbProcess, this->mInputNumBands, numInput, img); \
+	} \
+	else if (this->mOutputNumDimensions == 3) \
+	{ \
+		wrapName< imgType, 3 >::setNthInput( \
+				this->mOtbProcess, this->mInputNumBands, numInput, img); \
+	}\
+}
+
+/** ==============================================================
+ * 					internalLinkParameters
+ *  ==============================================================
+ */
+#define callInputTypeInternalLinkParameters( inputType, wrapName ) \
+{ \
+	if (this->mInputNumDimensions == 1) \
+	{ \
+		wrapName< inputType, 1 >::internalLinkParameters( \
+				this->mOtbProcess, this->mInputNumBands, \
+				this, step, repo); \
+	} \
+	else if (this->mInputNumDimensions == 2) \
+	{ \
+		wrapName< inputType, 2 >::internalLinkParameters( \
+				this->mOtbProcess, this->mInputNumBands, \
+				this, step, repo); \
+	} \
+	else if (this->mInputNumDimensions == 3) \
+	{ \
+		wrapName< inputType, 3 >::internalLinkParameters( \
+				this->mOtbProcess, this->mInputNumBands, \
+				this, step, repo); \
+	}\
+}
+
+/** ==============================================================
+ * 					internalExecute
+ *  ==============================================================
+ */
+#define callInputTypeInternalExecute( inputType, wrapName )	\
+{ \
+	if (this->mInputNumDimensions == 1) \
+	{ \
+		wrapName< inputType, 1 >::execute( \
+				this->mOtbProcess, this->mInputNumBands, this); \
+	} \
+	else if (this->mInputNumDimensions == 2) \
+	{ \
+		wrapName< inputType, 2 >::execute( \
+				this->mOtbProcess, this->mInputNumBands, this); \
+	} \
+	else if (this->mInputNumDimensions == 3) \
+	{ \
+		wrapName< inputType, 3 >::execute( \
+				this->mOtbProcess, this->mInputNumBands, this); \
+	}\
+}
 
 
-/**
- *  'OUTER' (ie INPUT) TYPE SWITCH MACRO
+// **********************************************************************************
+
+
+/*  ===============================================================================
+ *	  'OUTER' (ie INPUT) TYPE SWITCH MACRO
+ *	===============================================================================
  */
 
 #define MacroPerType( macroName, wrapName ) \
@@ -187,41 +328,9 @@ signals: \
 		macroName( double, wrapName );                                                \
 		break;
 
-//#define MacroPerType( macroName, wrapName ) \
-//	case itk::ImageIOBase::UCHAR:                                               \
-//		outputTypeSwitch( unsigned char, macroName, wrapName );                                         \
-//		break;                                                                  \
-//	case itk::ImageIOBase::CHAR:                                                \
-//		outputTypeSwitch( char, macroName, wrapName );                                                  \
-//		break;                                                                  \
-//	case itk::ImageIOBase::USHORT:                                              \
-//		outputTypeSwitch( unsigned short, macroName, wrapName );                                        \
-//		break;                                                                  \
-//	case itk::ImageIOBase::SHORT:                                               \
-//		outputTypeSwitch( short, macroName, wrapName );                                                 \
-//		break;                                                                  \
-//	case itk::ImageIOBase::UINT:                                                \
-//		outputTypeSwitch( unsigned int, macroName, wrapName );                                          \
-//		break;                                                                  \
-//	case itk::ImageIOBase::INT:                                                 \
-//		outputTypeSwitch( int, macroName, wrapName );                                                   \
-//		break;                                                                  \
-//	case itk::ImageIOBase::ULONG:                                               \
-//		outputTypeSwitch( unsigned long, macroName, wrapName );                                         \
-//		break;                                                                  \
-//	case itk::ImageIOBase::LONG:                                                \
-//		outputTypeSwitch( long, macroName, wrapName );                                                  \
-//		break;                                                                  \
-//	case itk::ImageIOBase::FLOAT:                                               \
-//		outputTypeSwitch( float, macroName, wrapName );                                                 \
-//		break;                                                                  \
-//	case itk::ImageIOBase::DOUBLE:                                              \
-//		outputTypeSwitch( double, macroName, wrapName );                                                \
-//		break;
-
-/**
+/*  ===============================================================================
  *  NESTED 'INNER' (ie OUTPUT) TYPE SWITCH MACRO
- *
+ *  ===============================================================================
  */
 #define outputTypeSwitch( inputType, macroName, wrapperClassName ) \
 switch(this->mOutputComponentType)	\
@@ -260,11 +369,31 @@ default:	\
 	break;	\
 }
 
+// **********************************************************************************
+
 
 /**
  *  VIRTUAL METHOD BODY MACROS for ever recurring functionality
  */
 
+#define LinkInputTypeInternalParametersWrap( ClassName, wrapName)		\
+void ClassName::linkParameters(unsigned int step,                       \
+		const QMap<QString, NMModelComponent*>& repo)                   \
+{                                                                       \
+	if (!this->mbIsInitialised)                                         \
+		return;                                                         \
+	switch(this->mInputComponentType)                                   \
+	{                                                                   \
+	MacroPerType( callInputTypeInternalLinkParameters, wrapName )       \
+	default:                                                            \
+		break;                                                          \
+	}                                                                   \
+}
+
+
+/** instantiation of process object, which is templated
+ *  over INPUT and OUTPUT types
+ */
 #define InstantiateObjectWrap( ClassName, wrapName )                                              \
 void ClassName::instantiateObject(void)        \
 {                                                                               \
@@ -311,6 +440,9 @@ void ClassName::instantiateObject(void)        \
 	NMDebugAI( << "... " << this->objectName().toStdString() << " - " << init << std::endl); \
 }
 
+/** instantiation of process object, which is templated
+ *  over OUTPUT type
+ */
 #define InstantiateOutputTypeObjectWrap( ClassName, WrapName)			\
 void ClassName::instantiateObject(void)                                 \
 {                                                                       \
@@ -319,7 +451,7 @@ void ClassName::instantiateObject(void)                                 \
 	bool init = true;                                                   \
 	switch (this->mOutputComponentType)                                 \
 	{                                                                   \
-	MacroPerType( callSingleTypeCreator, WrapName)                       \
+	MacroPerType( callOutputTypeCreator, WrapName)                       \
 	default:                                                            \
 		init = false;                                                   \
 		break;                                                          \
@@ -329,6 +461,30 @@ void ClassName::instantiateObject(void)                                 \
 			<< " - " << init << std::endl);                            \
 }
 
+/** instantiation of process object, which is templated
+ *  over INPUT type
+ */
+#define InstantiateInputTypeObjectWrap( ClassName, WrapName)			\
+void ClassName::instantiateObject(void)                                 \
+{                                                                       \
+	if (this->mbIsInitialised)                                          \
+	 return;                                                           \
+	bool init = true;                                                   \
+	switch (this->mInputComponentType)                                 \
+	{                                                                   \
+	MacroPerType( callInputTypeCreator, WrapName)                       \
+	default:                                                            \
+		init = false;                                                   \
+		break;                                                          \
+	}                                                                   \
+	this->mbIsInitialised = init;                                      \
+	NMDebugAI( << "... " << this->objectName().toStdString()            \
+			<< " - " << init << std::endl);                            \
+}
+
+/** get the output of the process object, which is templated
+ *  over the OUTPUT component type of this wrapper class
+ */
 #define GetOutputTypeOutputWrap( ClassName, InternalWrapper )                     \
 NMItkDataObjectWrapper* ClassName::getOutput(void)							      \
 {                                                                                 \
@@ -351,6 +507,34 @@ NMItkDataObjectWrapper* ClassName::getOutput(void)							      \
 	return dw;																	  \
 }
 
+/** get the output of the process object, which is templated
+ *  over the INPUT component type of this wrapper class
+ */
+#define GetInputTypeOutputWrap( ClassName, InternalWrapper )                     \
+NMItkDataObjectWrapper* ClassName::getOutput(void)							      \
+{                                                                                 \
+	if (!this->mbIsInitialised)                                                   \
+		return 0;                                                                 \
+                                                                                  \
+	itk::DataObject* img = 0;                                                     \
+                                                                                  \
+	switch (this->mInputComponentType)                                           \
+	{                                                                             \
+	MacroPerType( callInputTypeGetOutput, InternalWrapper )                       \
+	default:                                                                      \
+		break;                                                                    \
+	}                                                                             \
+                                                                                  \
+	NMItkDataObjectWrapper* dw =                                                  \
+			new NMItkDataObjectWrapper(this, img, this->mInputComponentType,     \
+			this->mInputNumDimensions, this->mInputNumBands);                   \
+                                                                                  \
+	return dw;																	  \
+}
+
+/** get the output of the process object, which is templated
+ *  over the INPUT and OUTPUT component type of this wrapper class
+ */
 #define GetOutputWrap( ClassName, InternalWrapper )                                              \
 NMItkDataObjectWrapper* ClassName::getOutput(void)								\
 {                                                                               \
@@ -402,6 +586,11 @@ NMItkDataObjectWrapper* ClassName::getOutput(void)								\
 	return dw;																	\
 }
 
+
+/** set the nth input of a process object, which is
+ *  templated over the INPUT and OUTPUT type of this
+ *  wrapper class
+ */
 #define SetNthInputWrap( ClassName, InternalWrapper )						\
 void ClassName::setNthInput(unsigned int numInput,  		\
 		NMItkDataObjectWrapper* imgWrapper)                 \
@@ -445,6 +634,26 @@ void ClassName::setNthInput(unsigned int numInput,  		\
 	default:                                                \
 		break;                                              \
 	}                                                       \
+}
+
+/** set the nth input of a process object, which is
+ *  templated over the INPUT type of this
+ *  wrapper class
+ */
+#define SetInputTypeNthInputWrap( ClassName, InternalWrapper )						\
+void ClassName::setNthInput(unsigned int numInput,  		\
+		NMItkDataObjectWrapper* imgWrapper)                 \
+{                                                           \
+	if (!this->mbIsInitialised)                             \
+		return;                                             \
+                                                            \
+	itk::DataObject* img = imgWrapper->getDataObject();     \
+	switch (this->mInputComponentType)                      \
+	{       \
+	MacroPerType( callInputTypeSetInput, InternalWrapper )                       \
+	default:                                                                      \
+		break;                                                                    \
+	}                                                                             \
 }
 
 

a07f4d0a8b03ddeb68b5eff6d2517af73fbf08de
Alexander Herzig <herziga@landcareresearch.co.nz>
2013-01-07 16:52:29 +1300

    enh: added NMMfwException class

---
+++ modellingframework/NMMfwException.h
@@ -0,0 +1,65 @@
+/******************************************************************************
+ * Created by Alexander Herzig
+ * Copyright 2013 Landcare Research New Zealand Ltd
+ *
+ * This file is part of 'LUMASS', which is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+/*
+ * NMMfwException.h
+ *
+ *  Created on: 6/01/2013
+ *      Author: alex
+ */
+
+#ifndef NMMFWEXCEPTION_H_
+#define NMMFWEXCEPTION_H_
+
+#include <string>
+#include <exception>
+#include <QObject>
+
+using namespace std;
+
+class NMMfwException: public std::exception
+{
+public:
+
+	typedef enum
+		{
+		  NMProcess_UninitialisedDataObject,
+		  NMProcess_UninitialisedProcessObject,
+		  NMProcess_InvalidInput,
+		  Unspecified
+
+		} ExceptionType;
+
+	NMMfwException(ExceptionType t = Unspecified);
+	virtual ~NMMfwException() throw();
+
+	virtual const char* what() const throw ();
+
+	void setMsg(const string& msg)
+	{
+		this->msg = msg;
+	}
+
+protected:
+	string msg;
+	ExceptionType type;
+
+	string type2string(void) const ;
+
+};
+
+#endif /* NMMFWEXCEPTION_H_ */

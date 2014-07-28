d5f974540ca67f3d4cc06f4d339ca1c54115ded1
Alex <herziga@landcareresearch.co.nz>
2014-07-29 06:23:08 +1200

    wip: packaging scripts

--- utils/fetchludeplib.sh
+++ utils/fetchludeplib.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # directory of lumass executable
-DIR=$1
-LUMASS_VERSION=$2 #0.9.32
-
+INDIR=$1
+OUTDIR=$2
+LUMASS_VERSION=$3 #0.9.32
 
 # locate and copy lumass dependencies into install tree
 # for distribution; we accept that some libs are tried
@@ -12,29 +12,36 @@ LUMASS_VERSION=$2 #0.9.32
 # so no harm done - just ignore admittledly ugly warnings ...
 
 #
-cd $DIR
+cd $INDIR
 
 # copy lumass depencies
-cp $( ldd lumass | cut -d " " -f 3) ../lib/
+cp $( ldd bin/lumass | cut -d " " -f 3) lib/
 
 # add platform specific libqxcb.so to file tree
-mkdir platforms
-cp $( locate libqxcb.so ) platforms/
+mkdir bin/platforms
+cp $( locate libqxcb.so ) bin/platforms/
 # also add all dependencies of libqxcb.so to lib dir in distr. tree
-cp $( ldd platforms/libqxcb.so | cut -d " " -f 3) ../lib/
+cp $( ldd bin/platforms/libqxcb.so | cut -d " " -f 3) lib/
 
 # need this one as well, so just copy it
-cp $( locate libQt5DBus.so ) ../lib/
+cp $( locate libQt5DBus.so ) lib/
 
 # also need to copy gdal kea plugin
-cp $( locate libkea.so ) ../lib/
-cp $( locate gdal_KEA.so) ../lib/
+LIBKEA=$( locate -n 1 libkea.so ) 
+cp $( ldd $( echo $LIBKEA ) | cut -d " " -f 3) lib/
+cp $( echo "$LIBKEA" ) lib/
+
+GDAL_KEA=$( locate -n 1 gdal_KEA.so)
+cp $( echo "$GDAL_KEA") lib/
 
 # package ...
-cd ../../
 
 OS=$( uname -s )
 REL=$( uname -r )
 PLATFORM=$( uname -i )
 
+cd ..
+
 tar czvf lumass-$( echo $LUMASS_VERSION )_$OS-$( echo $REL )_$PLATFORM.tar.gz lumass
+
+mv lumass-$( echo $LUMASS_VERSION )_$OS-$( echo $REL )_$PLATFORM.tar.gz "$OUTDIR"/

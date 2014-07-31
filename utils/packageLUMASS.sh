f7470e07e87ce291487a27d318b779b9c228d022
Super User <super@cally.landcare.ad.landcareresearch.co.nz>
2014-07-31 15:55:06 +1200

    enh: improve packaging and remove deprecated scripts

--- utils/packageLUMASS.sh
+++ utils/packageLUMASS.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
+#========== some functions
+bailout ()
+{
+	echo "ERROR: $MSG"
+	exit
+}
+#==========
+
+# check the number of arguments
+SYNOPSIS="Synopsis: $0 <lumass install dir> <package target dir> <lumass version>"
+if [ $# -ne 3 ]; then
+	MSG=$SYNOPSIS
+	bailout
+fi
 
 # get the directory for this script
 pushd `dirname $0` > /dev/null
 CURDIR=`pwd`
 popd > /dev/null
 
-# lumass install directory
-INDIR_=$1
-# make sure INDIR is an absolute path
-pushd `dirname $INDIR_` > /dev/null
-INDIR=`pwd`
-popd > /dev/null
-
-BASENAME=$( echo "$INDIR" | awk -F '/' '{print $NF}' )
-if [ -z $BASENAME ]; then
-	BASENAME=$( echo "$INDIR" | awk -F '/' '{print $(NF-1)}' )
+# check the lumass install directory
+if [ ! -d $1/lib -o ! -w $1/lib ]; then
+	MSG="Invalid or non-writable install directory specified!"
+	bailout	
 fi
 
-# package target directory
+INDIR_=$1
+cd $INDIR_
+INDIR=`pwd`
+cd $CURDIR
+
+# get the 'root' dir of the tar ball
+BASENAME=$( echo "$INDIR" | awk -F '/' '{print $NF}' )
+
+
+# check the package target directory
+if [ ! -d $2 -o ! -w $2 ]; then
+	MSG="Invalid or non-writable package target directory specified!"
+	bailout	
+fi
+
 OUTDIR_=$2
-# make sure OUTDIR is an absolute path
-pushd `dirname $OUTDIR_` > /dev/null
+cd $OUTDIR_
 OUTDIR=`pwd`
-popd > /dev/null
+cd $CURDIR
 
 # lumass version
 LUMASS_VERSION=$3 #0.9.32
@@ -55,15 +76,23 @@ cp $( locate libQt5DBus.so ) lib/
 
 # also need to copy gdal kea plugin
 LIBKEA=$( locate -n 1 libkea.so. ) 
-cp $( ldd $( echo $LIBKEA ) | cut -d " " -f 3) lib/
+cp $( ldd $( echo "$LIBKEA" ) | cut -d " " -f 3) lib/
 cp $( echo "$LIBKEA" ) lib/
 
 # create symlink to full version libkea
 LIBKEA2=$( echo "$LIBKEA" | awk -F '/' '{print $NF}' )
-ln -s lib/$LIBKEA2 lib/libkea.so
+if [ -a "lib/libkea.so" -o -L "lib/libkea.so" ]; then
+	rm lib/libkea.so
+fi
+
+echo "---> versioned libkea = $LIBKEA2"
+
+ln -s $( echo "$INDIR" )/lib/$( echo "$LIBKEA2" ) lib/libkea.so
+
+echo "---> created symlink lib/libkea.so ok"
 
 GDAL_KEA=$( locate -n 1 gdal_KEA.so)
-cp $( echo "$GDAL_KEA") lib/
+cp $( echo "$GDAL_KEA" ) lib/
 
 cd lib
 for LIB in $( ls ./ ); do

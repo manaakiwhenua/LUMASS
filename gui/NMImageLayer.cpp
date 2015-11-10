8de9a2a397ca145e451ff2f054fab04bf7bfb8be
Alexander Herzig <herziga@landcareresearch.co.nz>
2015-11-10 17:37:15 +1300

    wip: NMimageLayer -> create selection layer

--- gui/NMImageLayer.cpp
+++ gui/NMImageLayer.cpp
@@ -52,6 +52,7 @@
 #include "vtkImageResliceMapper.h"
 #include "vtkImageProperty.h"
 #include "vtkLookupTable.h"
+#include "vtkUnstructuredGrid.h"
 #include "vtkMath.h"
 #include "vtkDoubleArray.h"
 #include "vtkStringArray.h"
@@ -491,52 +492,60 @@ NMImageLayer::selectionChanged(const QItemSelection& newSel,
 		const QItemSelection& oldSel)
 {
 
-//    mSelectionMapper = vtkSmartPointer<vtkOGRLayerMapper>::New();
-//    vtkSmartPointer<vtkLookupTable> clrtab = vtkSmartPointer<vtkLookupTable>::New();
-//    vtkSmartPointer<vtkIdList> selCellIds = vtkSmartPointer<vtkIdList>::New();
-//    vtkSmartPointer<vtkLongArray> scalars = vtkSmartPointer<vtkLongArray>::New();
+    //    mSelectionMapper = vtkSmartPointer<vtkOGRLayerMapper>::New();
+    //    vtkSmartPointer<vtkLookupTable> clrtab = vtkSmartPointer<vtkLookupTable>::New();
+    vtkSmartPointer<vtkIdList> selCellIds = vtkSmartPointer<vtkIdList>::New();
+    //    vtkSmartPointer<vtkLongArray> scalars = vtkSmartPointer<vtkLongArray>::New();
 
-//    int selcnt = 0;
-//    foreach(const QItemSelectionRange& range, newSel)
-//    {
-//        const int top = range.top();
-//        const int bottom = range.bottom();
-//        for (int row=top; row<=bottom; ++row)
-//        {
-//            ++selcnt;
-//        }
-//    }
-//    selCellIds->SetNumberOfIds(selcnt);
-//    scalars->SetNumberOfTuples(selcnt);
-//    clrtab->SetNumberOfTableValues(selcnt);
+    int selcnt = 0;
+    foreach(const QItemSelectionRange& range, newSel)
+    {
+        const int top = range.top();
+        const int bottom = range.bottom();
+        for (int row=top; row<=bottom; ++row)
+        {
+            ++selcnt;
+        }
+    }
+    //    selCellIds->SetNumberOfIds(selcnt);
+    //    scalars->SetNumberOfTuples(selcnt);
+    //    clrtab->SetNumberOfTableValues(selcnt);
 
 //    //this->printSelRanges(newSel, "incoming update selection");
 
-//    int clrcnt = 0;
-//    foreach(const QItemSelectionRange& range, newSel)
-//    {
-//        const int top = range.top();
-//        const int bottom = range.bottom();
-//        for (int row=top; row<=bottom; ++row)
-//        {
-//            scalars->SetValue(clrcnt, clrcnt);
-//            selCellIds->SetId(clrcnt, row);
-//            clrtab->SetTableValue(clrcnt, 1, 0, 0);
-//            ++clrcnt;
-//        }
-//    }
+    int clrcnt = 0;
+    foreach(const QItemSelectionRange& range, newSel)
+    {
+        const int top = range.top();
+        const int bottom = range.bottom();
+        for (int row=top; row<=bottom; ++row)
+        {
+            //scalars->SetValue(clrcnt, clrcnt);
+            selCellIds->SetId(clrcnt, row);
+            //clrtab->SetTableValue(clrcnt, 1, 0, 0);
+            ++clrcnt;
+        }
+    }
 
-//    //NMDebugAI(<< "we should have " << selCellIds->GetNumberOfIds() << " extracted cells" << std::endl);
+    //vtkImageData* id = vtkImageData::SafeDownCast(mDataSet);
+    //id->getP
 
-//    if (this->mCellSelection.GetPointer() != 0 && mSelectionActor.GetPointer() != 0)
-//    {
-//        NMDebugAI(<< "removed old selection" << std::endl);
-//        this->mRenderer->RemoveActor(mSelectionActor);
-//    }
+    NMDebugAI(<< "we should have " << selCellIds->GetNumberOfIds() << " extracted cells" << std::endl);
 
-//    vtkSmartPointer<vtkExtractCells> extractor = vtkSmartPointer<vtkExtractCells>::New();
-//    extractor->SetInputData(mDataSet);
-//    extractor->SetCellList(selCellIds);
+    //    if (this->mCellSelection.GetPointer() != 0 && mSelectionActor.GetPointer() != 0)
+    //    {
+    //        NMDebugAI(<< "removed old selection" << std::endl);
+    //        this->mRenderer->RemoveActor(mSelectionActor);
+    //    }
+
+    vtkSmartPointer<vtkExtractCells> extractor = vtkSmartPointer<vtkExtractCells>::New();
+    extractor->SetInputData(mDataSet);
+    extractor->SetCellList(selCellIds);
+    extractor->Update();
+
+    vtkSmartPointer<vtkUnstructuredGrid> ugrid = extractor->GetOutput();
+
+    //ugrid->GetP
 
 //    vtkSmartPointer<vtkGeometryFilter> geoFilter = vtkSmartPointer<vtkGeometryFilter>::New();
 //    geoFilter->SetInputConnection(extractor->GetOutputPort());

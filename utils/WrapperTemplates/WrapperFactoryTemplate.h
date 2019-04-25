e01c05289f7690872d80bc819a61354ba436c1db
Alex Herzig <herziga@landcareresearch.co.nz>
2019-04-25 18:19:49 +1200

    enh: utils/WrapperTemplates      - added WrapperFactoryTemplate.cpp ~.h      - added makeWrapperFactory.py to facilitate        factory generation
    utils/WrapperTemplates/WrapperFactoryTemplate.cpp
    utils/WrapperTemplates/WrapperFactoryTemplate.h
    utils/WrapperTemplates/makeWrapperFactory.py

---
+++ utils/WrapperTemplates/WrapperFactoryTemplate.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * Created by Alexander Herzig
+ * Copyright /*$<Year>$*/ Landcare Research New Zealand Ltd
+ *
+ * This file is part of 'LUMASS', which is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+/*
+ * /*$<WrapperFactoryName>$*/.h
+ *
+ *  Created on: /*$<FileDate>$*/
+ *      Author: /*$<Author>$*/
+ */
+
+#ifndef /*$<WrapperFactoryName>$*/_H_
+#define /*$<WrapperFactoryName>$*/_H_
+
+#include <QObject>
+#include "NMWrapperFactory.h"
+
+#include "/*$<WrapperClassNameLower>$*/_export.h"
+
+class /*$<WrapperClassNameUpper>$*/_EXPORT /*$<WrapperFactoryName>$*/ : public NMWrapperFactory
+{
+    Q_OBJECT
+public:
+    /*$<WrapperFactoryName>$*/(QObject *parent = nullptr);
+
+    NMProcess* createWrapper();
+    bool isSinkProcess(void) {return /*$<ProcessIsSink>$*/;}
+    QString getWrapperClassName() {return "/*$<WrapperClassName>$*/";}
+};
+
+#endif // /*$<WrapperFactoryName>$*/_H
---
+++ utils/WrapperTemplates/makeWrapperFactory.py
@@ -0,0 +1,182 @@
+import sys
+import shutil
+import inspect, os
+import string
+import re
+import errno
+
+
+
+# ===============================================================================
+if __name__ == '__main__':
+    '''
+    open the files, to the copy, and do the main
+    workflow here, then we'll break out to do some things
+    more special
+    '''
+    if len(sys.argv) < 11:
+        print "Usage: $ %s makeWrapperFactory --cl_name <WrapperClassName> --isSink <true | false> --author <author> --year <year> --date <date>"
+        sys.exit()
+
+    pdict = {}
+
+    for i in range(1, len(sys.argv)):
+        print "processing '%s' ..." % sys.argv[i]
+
+        if sys.argv[i] == "--cl_name":
+            pdict['cl_name'] = sys.argv[i+1]
+            pdict['cl_name_lower'] = pdict['cl_name'].lower()
+            pdict['cl_name_upper'] = pdict['cl_name'].upper()
+
+            pdict['fac_name'] = sys.argv[i+1] + 'Factory'
+        elif sys.argv[i] == "--isSink":
+            if sys.argv[i+1] == "true" or sys.argv[i+1] == "false":
+                pdict['isSink'] = sys.argv[i+1]
+            else:
+                pdict['isSink'] = "false"
+        elif sys.argv[i] == "--author":
+            pdict['author'] = sys.argv[i+1]
+        elif sys.argv[i] == "--year":
+            pdict['year'] = sys.argv[i+1]
+        elif sys.argv[i] == "--date":
+            pdict['date'] = sys.argv[i+1]
+
+    # test - debug
+    #for key in pdict:
+    #    print "%s = %s" % (key, pdict[key])
+
+    searchDict = {}
+    searchDict['fac_name'] = "/*$<WrapperFactoryName>$*/"
+    searchDict['cl_name'] = "/*$<WrapperClassName>$*/"
+    searchDict['cl_name_lower'] = "/*$<WrapperClassNameLower>$*/"
+    searchDict['cl_name_upper'] = "/*$<WrapperClassNameUpper>$*/"
+    searchDict['fac_name'] = "/*$<WrapperFactoryName>$*/"
+    searchDict['author'] = "/*$<Author>$*/"
+    searchDict['isSink'] = "/*$<ProcessIsSink>$*/"
+    searchDict['year'] = "/*$<Year>$*/"
+    searchDict['date'] = "/*$<FileDate>$*/"
+
+    # =======================================================
+    # create new wrapper factory class files from template files
+
+    factoryClassName = pdict['fac_name']
+
+    filepath = inspect.getfile(inspect.currentframe())
+    path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    if os.sep == '/':
+        pos = string.rfind(path, '/', 0)
+        pos = string.rfind(path, '/', 0, pos)
+    else:
+        pos = string.rfind(path, '\\', 0)
+        pos = string.rfind(path, '\\', 0, pos)
+
+    homepath = path[0:pos]
+
+    frameworkpath = os.path.join(homepath, 'modellingframework')
+    targetpath = os.path.join(frameworkpath, 'wrapper')
+    print "    >>> lumass HOME=%s" % homepath
+    print "    >>> wrapper file path: %s" % targetpath
+
+    inCppPath = os.path.join(path, 'WrapperFactoryTemplate.cpp')
+    inHPath   = os.path.join(path, 'WrapperFactoryTemplate.h')
+
+    print "   >>> inCppPath=%s" % inCppPath
+    print "   >>> inHPath=%s" % inHPath
+
+    classNameCPP = "%s.cpp" % (factoryClassName)
+    classNameH = "%s.h" % (factoryClassName)
+    cppPath = os.path.join(targetpath, classNameCPP)
+    hPath = os.path.join(targetpath, classNameH)
+
+    print "   >>> cppPath=%s" % cppPath
+    print "   >>> hPath=%s" % hPath
+
+
+    shutil.copyfile(inHPath, hPath)
+    shutil.copyfile(inCppPath, cppPath)
+
+
+    # =======================================================
+    # process header file
+
+    # HEADER FILE
+    hStr = None
+    with open(hPath, 'r') as hWrapper:
+        hStr = hWrapper.read()
+
+        for key in pdict:
+            hStr = hStr.replace(searchDict[key], pdict[key])
+
+        if hStr.find("/*$<") != -1:
+            print "WARNING: There's likely one or more unreplaced wrapper keywords left in %s!" % (hPath)
+
+    with open(hPath, 'w') as hWrapper:
+        hWrapper.write(hStr)
+
+
+
+    # CPP file
+    cppStr = None
+    with open(cppPath, 'r') as cppWrapper:
+        cppStr = cppWrapper.read()
+
+        for key in pdict:
+            cppStr = cppStr.replace(searchDict[key], pdict[key])
+
+        if cppStr.find("/*$<") != -1:
+            print "WARNING: There's likely one or more unreplaced wrapper keywords left in %s!" % (cppPath)
+
+
+    with open(cppPath, 'w') as cppWrapper:
+        cppWrapper.write(cppStr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

e6a725592fadcee74e5f29db85db58d5345f177c
Alex Herzig <herziga@landcareresearch.co.nz>
2021-05-05 16:58:53 +1200

    rev: rev. LogError statement: explicitly use std::endl with namespace
    gui/NMVectorLayer.cpp

--- gui/NMVectorLayer.cpp
+++ gui/NMVectorLayer.cpp
@@ -22,8 +22,6 @@
  *      Author: alex
  */
 
-//#include "NMTableView.h"
-
 #ifndef NM_ENABLE_LOGGER
 #   define NM_ENABLE_LOGGER
 #   include "nmlog.h"
@@ -46,9 +44,7 @@
 #include "vtkPolyDataWriter.h"
 #include "vtkLookupTable.h"
 #include "vtkTriangleFilter.h"
-#include "vtkOGRLayerMapper.h"
-#include "vtkPolyDataMapper.h"
-//#include "vtkOpenGLPolyDataMapper.h"
+
 #include "vtkCellData.h"
 #include "vtkCellArray.h"
 #include "vtkPolygon.h"
@@ -59,7 +55,6 @@
 #include "vtkDoubleArray.h"
 #include "vtkStringArray.h"
 #include "vtkSmartPointer.h"
-//#include "vtkLODActor.h"
 
 #include "vtkQtEditableTableModelAdapter.h"
 #include "vtkQtTableModelAdapter.h"
@@ -71,13 +66,10 @@
 #include "vtkRowQueryToTable.h"
 #include "vtkSQLiteDatabase.h"
 #include "vtkProperty.h"
-//#include "vtkDepthSortPolyData.h"
 #include "vtkExtractCells.h"
 #include "vtkGeometryFilter.h"
 #include "vtkDataSetMapper.h"
 
-#include "vtkOpenGLPolyDataMapper.h"
-#include "NMVtkOpenGLPolyDataMapper.h"
 #include "NMPolygonToTriangles.h"
 
 
@@ -252,93 +244,23 @@ void NMVectorLayer::setDataSet(vtkDataSet* dataset)
 	// set the bounding box
 	pd->GetBounds(this->mBBox);
 
-	//mDepthSort = vtkSmartPointer<vtkDepthSortPolyData>::New();
-	//mDepthSort->SetInput(pd);
-	////sortof->SetDirectionToBackToFront();
-	////sortof->SetVector(1,1,1);
-	//mDepthSort->SetCamera(this->mRenderer->GetActiveCamera());
-	//mDepthSort->SortScalarsOn();
-
 	// create and set the mapper
 #ifdef VTK_OPENGL2
-    vtkSmartPointer<NMVtkOpenGLPolyDataMapper> m = vtkSmartPointer<NMVtkOpenGLPolyDataMapper>::New();
-
-//    QString vscodeFN = "/home/alex/garage/cpp/lumass/utils/NMVtk/vtkPolyDataVS.glsl";
-//    QFile vscodeFile(vscodeFN);
-//    vscodeFile.open(QIODevice::ReadOnly | QIODevice::Text);
-//    QTextStream vsstr(&vscodeFile);
-//    QString vscode = vsstr.readAll();
-//    m->SetVertexShaderCode(vscode.toStdString().c_str());
-//    vscodeFile.close();
-
-//    QString cscodeFN = "/home/alex/garage/cpp/lumass/utils/NMVtk/vtkPolyDataCS.glsl";
-//    QFile cscodeFile(cscodeFN);
-//    cscodeFile.open(QIODevice::ReadOnly | QIODevice::Text);
-//    QTextStream csstr(&cscodeFile);
-//    QString cscode = csstr.readAll();
-//    m->SetTessControlShaderCode(cscode.toStdString().c_str());
-//    cscodeFile.close();
-
-//    QString escodeFN = "/home/alex/garage/cpp/lumass/utils/NMVtk/vtkPolyDataES.glsl";
-//    QFile escodeFile(escodeFN);
-//    escodeFile.open(QIODevice::ReadOnly | QIODevice::Text);
-//    QTextStream esstr(&escodeFile);
-//    QString escode = esstr.readAll();
-//    m->SetTessEvaluationShaderCode(escode.toStdString().c_str());
-//    escodeFile.close();
-
-//    QString fscodeFN = "/home/users/herziga/garage/cpp/lumass/utils/NMVtk/vtkPolyDataFS.glsl";
-//    QFile fscodeFile(fscodeFN);
-//    fscodeFile.open(QIODevice::ReadOnly | QIODevice::Text);
-//    QTextStream fsstr(&fscodeFile);
-//    QString fscode = fsstr.readAll();
-//    m->SetFragmentShaderCode(fscode.toStdString().c_str());
-//    fscodeFile.close();
-
-//    vtkSmartPointer<vtkTriangleFilter> tf = vtkSmartPointer<vtkTriangleFilter>::New();
-//    tf->SetInputData(pd);
-//    m->SetInputConnection(tf->GetOutputPort());
-
-//    vtkSmartPointer<NMPolygonToTriangles> tess = vtkSmartPointer<NMPolygonToTriangles>::New();
-//    tess->SetInputData(pd);
-//    tess->Update();
-//    pd = tess->GetOutput();
-//    this->mDataSet = pd;
-//    //m->SetInputData(tess->GetOutput());
-
-
-//    std::vector<vtkIdType> ids = tess->GetPolyIdMap();
-//    std::cout << "tris id -> poly id ...\n";
-//    for (int i=0; i < ids.size(); ++i)
-//    {
-//        std::cout << "   " << i << "    -> " << ids.at(i) << "\n";
-//    }
-
-
+    vtkSmartPointer<NMVtkOpenGLPolyDataMapper2> m = vtkSmartPointer<NMVtkOpenGLPolyDataMapper2>::New();
     m->SetInputData(pd);
 #else
     vtkSmartPointer<vtkOGRLayerMapper> m = vtkSmartPointer<vtkOGRLayerMapper>::New();
     m->SetInputData(pd);
 #endif
 
-//    m->SetInputData(pd);
-	//m->SetInputConnection(mDepthSort->GetOutputPort());
-	//m->SetScalarRange(0, mDepthSort->GetOutput()->GetNumberOfCells());
 	this->mMapper = m;
 
 	// create and set the actor
     vtkSmartPointer<vtkActor> a = vtkSmartPointer<vtkActor>::New();
 	a->SetMapper(m);
-	//a->GetProperty()->SetOpacity(0.3);
-	//mDepthSort->SetProp3D(a);
-    //a->GetProperty()->SetRepresentation(VTK_WIREFRAME);
 	this->mActor = a;
 	this->mActor->SetVisibility(0);
-
-
-	//vtkActor* actor = vtkActor::SafeDownCast(this->mActor);
-	//actor->GetProperty()->SetOpacity(0.2);
-	this->mRenderer->AddActor(a);
+    this->mRenderer->AddActor(a);
 
 	// create contours, if we've got polygons
     if (this->mFeatureType == NMVectorLayer::NM_POLYGON_FEAT)
@@ -350,7 +272,6 @@ void NMVectorLayer::setDataSet(vtkDataSet* dataset)
         cont->SetLines(pd->GetPolys());
         this->setContour(cont);
     }
-//    this->mContour = nullptr;
 
 	this->initiateLegend();
 
@@ -462,229 +383,6 @@ long NMVectorLayer::getNumberOfFeatures(void)
 }
 
 
-//void
-//NMVectorLayer::mapUniqueValues()
-//{
-//	if (this->mFeatureType != NMVectorLayer::NM_POLYGON_FEAT)
-//	{
-//		NMLayer::mapUniqueValues();
-//		return;
-//	}
-//
-//	// make a list of available attributes
-//	vtkDataSet* ds = const_cast<vtkDataSet*>(this->getDataSet());
-//	vtkDataSetAttributes* dsAttr = ds->GetAttributes(vtkDataSet::CELL);
-//	vtkUnsignedCharArray* hole = vtkUnsignedCharArray::SafeDownCast(
-//			dsAttr->GetArray("nm_hole"));
-//	vtkLongArray* nmid = vtkLongArray::SafeDownCast(
-//			dsAttr->GetArray("nm_id"));
-//
-//	// let's find out about the attribute
-//	// if we've got doubles, we refuse to map unique values ->
-//	// doesn't make sense, does it?
-//	vtkAbstractArray* anAr = dsAttr->GetAbstractArray(mLegendValueField.toStdString().c_str());
-//	int type = anAr->GetDataType();
-//	if (type == VTK_DOUBLE)
-//	{
-//		NMDebugAI( << "oh no, not with doubles!" << endl);
-//		return;
-//	}
-//
-//	bool bNum = true;
-//	if (type == VTK_STRING)
-//		bNum = false;
-//
-//	const long ncells = anAr->GetNumberOfTuples();
-//
-//	// we create a new look-up table and set the number of entries we need
-//	mLookupTable = vtkSmartPointer<vtkLookupTable>::New();
-//	mLookupTable->SetNumberOfTableValues(ncells);
-//
-//	// let's create a new legend info table
-//	this->resetLegendInfo();
-//
-//	// we iterate over the number of tuples in the user specified attribute array
-//	// and assign each unique categorical value its own (hopefully unique)
-//	// random colour, which is then inserted into the layer's lookup table; we further
-//	// specify a default name for each colour and put it together with the
-//	// chosen colour into a LengendInfo-Table, which basically holds the legend
-//	// category to display; for linking attribute values to table-info and lookup-table
-//	// indices, we fill the HashMap mHashValueIndices (s. Header file for further descr.)
-//	bool bConvOk;
-//	int clrCount = 0, val;
-//	QString sVal;
-//	vtkMath::RandomSeed(QTime::currentTime().msec());
-//	double rgba[4];
-//	for (int t=0; t < ncells; ++t)
-//	{
-//		if (hole->GetValue(t))
-//		{
-//			mLookupTable->SetTableValue(t, rgba[0], rgba[1], rgba[2]);
-//			continue;
-//		}
-//
-//		if (bNum)
-//		{
-//			int val = anAr->GetVariantValue(t).ToInt(&bConvOk);
-//			sVal = QString(tr("%1")).arg(val);
-//		}
-//		else
-//		{
-//			sVal = anAr->GetVariantValue(t).ToString().c_str();
-//		}
-//
-//		QHash<QString, QVector<int> >::iterator it = this->mHashValueIndices.find(sVal);
-//		if (it == this->mHashValueIndices.end())
-//		{
-//			// add the key value pair to the hash map
-//			QVector<int> idxVec;
-//			idxVec.append(clrCount);
-//			this->mHashValueIndices.insert(sVal, idxVec);
-//
-//			// add a new row to the legend_info table
-//			vtkIdType newidx = this->mLegendInfo->InsertNextBlankRow(-9);
-//
-//			// add the value to the index map
-//			double lowup[2];
-//			lowup[0] = val;
-//			lowup[1] = val;
-//
-//			vtkDoubleArray* lowupAbstrAr = vtkDoubleArray::SafeDownCast(
-//					this->mLegendInfo->GetColumnByName("range"));
-//			lowupAbstrAr->SetTuple(newidx, lowup);
-//
-//			// generate a random triple of uchar values
-//			for (int i=0; i < 3; i++)
-//				rgba[i] = vtkMath::Random();
-//			rgba[3] = 1;
-//
-//			// add the color spec to the colour map
-//			vtkDoubleArray* rgbaAr = vtkDoubleArray::SafeDownCast(
-//					this->mLegendInfo->GetColumnByName("rgba"));
-//			rgbaAr->SetTuple(newidx, rgba);
-//
-//			// add the name (sVal) to the name column of the legendinfo table
-//			vtkStringArray* nameAr = vtkStringArray::SafeDownCast(
-//					this->mLegendInfo->GetColumnByName("name"));
-//			nameAr->SetValue(newidx, sVal.toStdString().c_str());
-//
-//			// add the color spec to the mapper's color table
-//			mLookupTable->SetTableValue(t, rgba[0], rgba[1], rgba[2], rgba[3]);
-//			//			NMDebugAI( << clrCount << ": " << sVal.toStdString() << " = " << rgba[0]
-//			//					<< " " << rgba[1] << " " << rgba[2] << endl);
-//
-//			clrCount++;
-//		}
-//		else
-//		{
-//			// add the index to the index map
-//			int tabInfoIdx = this->mHashValueIndices.find(sVal).value()[0];
-//			this->mHashValueIndices.find(sVal).value().append(t);
-//
-//			// add the colour to the real color table
-//			vtkDoubleArray* dblAr = vtkDoubleArray::SafeDownCast(this->mLegendInfo->GetColumnByName("rgba"));
-//			//double tmprgba[4];
-//			dblAr->GetTuple(tabInfoIdx, rgba);
-//
-//			mLookupTable->SetTableValue(t, rgba[0], rgba[1], rgba[2]);
-//		}
-//	}
-//
-//	mNumLegendRows = mLegendInfo->GetNumberOfRows() + 2;
-//	mLegendDescrField = mLegendValueField;
-//
-//	//// get the mapper and look whats possible
-//	//vtkMapper* mapper = vtkMapper::SafeDownCast(this->mMapper);
-//	//mapper->SetLookupTable(mLookupTable);
-//	//mapper->Modified();
-//    //
-//	//emit visibilityChanged(this);
-//	//emit legendChanged(this);
-//
-//	return;
-//}
-
-
-//void NMVectorLayer::mapSingleSymbol()
-//{
-//	NMDebugCtx(ctxNMVectorLayer, << "...");
-//	if (this->mFeatureType != NMVectorLayer::NM_POLYGON_FEAT)
-//	{
-//		NMLayer::mapSingleSymbol();
-//		NMDebugCtx(ctxNMVectorLayer, << "done!");
-//		return;
-//	}
-//
-//	vtkDataSetAttributes* dsAttr = this->mDataSet->GetAttributes(vtkDataSet::CELL);
-//	vtkLongArray* nmids = vtkLongArray::SafeDownCast(dsAttr->GetArray("nm_id"));
-//	long ncells = nmids->GetNumberOfTuples();
-//
-//	vtkUnsignedCharArray* hole = vtkUnsignedCharArray::SafeDownCast(dsAttr->GetArray("nm_hole"));
-//
-//	// we create a new look-up table and set the number of entries we need
-//	mLookupTable = vtkSmartPointer<vtkLookupTable>::New();
-//	mLookupTable->SetNumberOfTableValues(ncells);
-//
-//	mNumLegendRows = 2;
-//	mLegendDescrField = tr("All Features");
-//
-//	// let's create a new legend info table
-//	//this->resetLegendInfo();
-//
-//	// chose a random colour
-//	QString sVal;
-//	vtkMath::RandomSeed(QTime::currentTime().msec());
-//
-//	double rgba[4];
-//	for (int i=0; i < 3; i++)
-//		rgba[i] = vtkMath::Random();
-//	rgba[3] = 1;
-//
-//	// cell index vector for has map
-//	//QVector<int> idxVec;
-//
-//	// fill colour look-up table for mapper
-//	for (long l=0; l < ncells; ++l)
-//	{
-//		// add the new cell index to the hash map
-//		//idxVec.append(l);
-//
-//		//if (hole->GetValue(l))
-//		//{
-//		//	//mLookupTable->SetTableValue(l, rgba[0], rgba[1], rgba[2], 0);
-//		//}
-//		//else
-//		{
-//			mLookupTable->SetTableValue(l, rgba[0], rgba[1], rgba[2], rgba[3]);
-//		}
-//	}
-//
-//	//// fill hash map
-//	//vtkIdType newidx = this->mLegendInfo->InsertNextBlankRow(-9);
-//	//this->mHashValueIndices.insert(tr(""), idxVec);
-//    //
-//    //
-//	//// fill mLegendInfoTable with corresponding infor
-//	//double lowup[2];
-//	//lowup[0] = -9;
-//	//lowup[1] = -9;
-//    //
-//	//vtkDoubleArray* lowupAbstrAr = vtkDoubleArray::SafeDownCast(
-//	//		this->mLegendInfo->GetColumnByName("range"));
-//	//lowupAbstrAr->SetTuple(newidx, lowup);
-//    //
-//	//vtkStringArray* nameAr = vtkStringArray::SafeDownCast(
-//	//		this->mLegendInfo->GetColumnByName("name"));
-//	//nameAr->SetValue(newidx, "");
-//    //
-//	//vtkDoubleArray* clrs = vtkDoubleArray::SafeDownCast(
-//	//		this->mLegendInfo->GetColumnByName("rgba"));
-//	//clrs->SetTuple(newidx, rgba);
-//
-//	NMDebugCtx(ctxNMVectorLayer, << "done!");
-//
-//}
-
 void NMVectorLayer::createTableView(void)
 {
 	if (this->mTableView != 0)
@@ -730,20 +428,6 @@ void NMVectorLayer::createTableView(void)
 
 	connect(this, SIGNAL(selectabilityChanged(bool)), mTableView, SLOT(setSelectable(bool)));
     connect(mTableView, SIGNAL(notifyLastClickedRow(long long)), this, SLOT(forwardLastClickedRowSignal(long long)));
-
-	// connect tableview signals to layer slots
-	//this->connect(this->mTableView,
-	//		SIGNAL(tableDataChanged(QStringList&, QStringList&)), this,
-	//		SLOT(emitAttributeTableChanged(QStringList&, QStringList&)));
-	//this->connect(this->mTableView, SIGNAL(selectionChanged()), this,
-	//		SLOT(updateSelectionData()));
-
-	// connect layer signals to tableview slots
-	//this->connect(this, SIGNAL(attributeTableChanged(vtkTable*)),
-	//		this->mTableView, SLOT(setTable(vtkTable*)));
-//	this->connect(this, SIGNAL(layerSelectionChanged(NMLayer*)),
-//			this->mTableView, SLOT(updateSelection()));
-
 }
 
 int NMVectorLayer::updateAttributeTable(void)
@@ -793,74 +477,6 @@ int NMVectorLayer::updateAttributeTable(void)
 	return 1;
 }
 
-//const vtkTable* NMVectorLayer::getTable(void)
-//{
-//	if (this->mAttributeTable.GetPointer() == 0)
-//		this->updateAttributeTable();
-//
-//	return this->mAttributeTable;
-//}
-
-//void NMVectorLayer::updateDataSet(QStringList& slAlteredColumns,
-//		QStringList& slDeletedColumns)
-//{
-//	NMDebugCtx(ctxNMVectorLayer, << "...");
-//
-//	if (slDeletedColumns.size() == 0 && slAlteredColumns.size() == 0)
-//	{
-//		NMDebugAI(<< "nothing to save!" << endl);
-//		NMDebugCtx(ctxNMVectorLayer, << "done!");
-//		return;
-//	}
-//
-//	vtkDataSetAttributes* dsAttr = this->mDataSet->GetAttributes(vtkDataSet::CELL);
-//
-//	vtkUnsignedCharArray* holeAr;
-//	if (this->mFeatureType == NMVectorLayer::NM_POLYGON_FEAT)
-//		holeAr = vtkUnsignedCharArray::SafeDownCast(dsAttr->GetArray("nm_hole"));
-//
-//
-//	// delete all deleted columns
-//	for (int d=0; d < slDeletedColumns.size(); ++d)
-//		dsAttr->RemoveArray(slDeletedColumns.at(d).toStdString().c_str());
-//
-//	// add / update columns
-//	for (int a=0; a < slAlteredColumns.size(); ++a)
-//	{
-//		vtkAbstractArray* aa = this->mAttributeTable->GetColumnByName(
-//				slAlteredColumns.at(a).toStdString().c_str());
-//
-//		vtkSmartPointer<vtkAbstractArray> na = vtkAbstractArray::CreateArray(aa->GetDataType());
-//		na->SetName(aa->GetName());
-//		na->SetNumberOfComponents(1);
-//		na->Allocate(holeAr->GetNumberOfTuples());
-//
-//		int aaCnt = 0;
-//		for (int r=0; r < holeAr->GetNumberOfTuples(); ++r)
-//		{
-//			if (holeAr->GetValue(r))
-//			{
-//				na->InsertVariantValue(r, vtkVariant(0));
-//			}
-//			else
-//			{
-//				na->InsertVariantValue(r, vtkVariant(aa->GetVariantValue(aaCnt)));
-//				++aaCnt;
-//			}
-//		}
-//
-//		dsAttr->AddArray(na);
-//	}
-//
-//	slDeletedColumns.clear();
-//	slAlteredColumns.clear();
-//
-//	//emit dataSetChanged(this);
-//	//emit legendChanged(this);
-//
-//	NMDebugCtx(ctxNMVectorLayer, << "done!");
-//}
-
 void NMVectorLayer::writeDataSet(void)
 {
 	NMDebugCtx(ctxNMVectorLayer, << "...");
@@ -869,7 +485,7 @@ void NMVectorLayer::writeDataSet(void)
 	if (!finfo.isWritable())
 	{
         NMLogError(<< ctxNMVectorLayer << ": can't write to '" << this->mFileName.toStdString() <<
-				"' - check permissions!" << endl);
+				"' - check permissions!" << std::endl);
 		return;
 	}
 
@@ -1037,137 +653,3 @@ NMVectorLayer::updateSelectionColor()
 
     this->mSelectionMapper->Update();
 }
-
-//void NMVectorLayer::updateSelectionData(void)
-//{
-//	NMDebugCtx(ctxNMVectorLayer, << "...");
-//
-//	vtkDataSetAttributes* dsAttr = this->mDataSet->GetAttributes(vtkDataSet::CELL);
-//
-//	vtkUnsignedCharArray* holeAr;
-//	if (this->mFeatureType == NMVectorLayer::NM_POLYGON_FEAT)
-//		holeAr = vtkUnsignedCharArray::SafeDownCast(dsAttr->GetArray("nm_hole"));
-//
-//	vtkDataArray* dsSel = dsAttr->GetArray("nm_sel");
-//
-//	vtkTable* tab = const_cast<vtkTable*>(this->mTableView->getTable());
-//
-//	vtkDataArray* tabSel = vtkDataArray::SafeDownCast(
-//					tab->GetColumnByName("nm_sel"));
-//
-//	vtkLookupTable* clrTab = vtkLookupTable::SafeDownCast(
-//			this->mContourMapper->GetLookupTable());
-//
-//	int valCellCnt = 0;
-//	for (int r = 0; r < holeAr->GetNumberOfTuples(); ++r)
-//	{
-//		if (holeAr->GetValue(r) == 1)
-//		{
-//			clrTab->SetTableValue(r, 0,0,0,1);
-//			continue;
-//		}
-//
-//		dsSel->SetTuple1(r, tabSel->GetTuple1(valCellCnt));
-//
-//		if (tabSel->GetTuple1(valCellCnt) != 0)
-//			clrTab->SetTableValue(r, 1,0,0,1);
-//		else
-//			clrTab->SetTableValue(r, 0,0,0,1);
-//
-//		++valCellCnt;
-//	}
-//
-//	emit visibilityChanged(this);
-//	emit legendChanged(this);
-//
-//	NMDebugCtx(ctxNMVectorLayer, << "done!");
-//}
-
-//void NMVectorLayer::updateLayerSelection(QList<long> lstCellId,
-//		QList<long> lstNMId, NMLayerSelectionType seltype)
-//{
-//	// update the table, if present
-//	if (this->mTableView != 0)
-//	{
-//		vtkDataArray* tabSel = vtkDataArray::SafeDownCast(
-//				this->mAttributeTable->GetColumnByName("nm_sel"));
-//
-//		switch (seltype)
-//		{
-//		case NM_SEL_NEW:
-//			this->mTableView->clearSelection();
-//			foreach(const int &nmid, lstNMId)
-//				tabSel->SetTuple1(nmid-1, 1);
-//			break;
-//		case NM_SEL_ADD:
-//			foreach(const int &nmid, lstNMId)
-//				tabSel->SetTuple1(nmid-1, 1);
-//			break;
-//		case NM_SEL_REMOVE:
-//			foreach(const int &nmid, lstNMId)
-//				tabSel->SetTuple1(nmid-1, 0);
-//			break;
-//		case NM_SEL_CLEAR:
-//			this->mTableView->clearSelection();
-//			break;
-//		}
-//
-//		emit attributeTableChanged(this->mAttributeTable);
-//	}
-//
-//	// update the data set and selection
-//	vtkDataSetAttributes* dsAttr = this->mDataSet->GetAttributes(vtkDataSet::CELL);
-//
-//	vtkUnsignedCharArray* holeAr;
-//	if (this->mFeatureType == NMVectorLayer::NM_POLYGON_FEAT)
-//		holeAr = vtkUnsignedCharArray::SafeDownCast(dsAttr->GetArray("nm_hole"));
-//	vtkDataArray* dsSel = dsAttr->GetArray("nm_sel");
-//
-//	vtkLookupTable* clrTab = vtkLookupTable::SafeDownCast(
-//			this->mContourMapper->GetLookupTable());
-//
-//	switch(seltype)
-//	{
-//	case NM_SEL_NEW:
-//		for (int r = 0; r < holeAr->GetNumberOfTuples(); ++r)
-//		{
-//			clrTab->SetTableValue(r, 0,0,0,1);
-//			dsSel->SetTuple1(r,0);
-//		}
-//
-//		foreach(const int &cid, lstCellId)
-//		{
-//			dsSel->SetTuple1(cid,1);
-//			clrTab->SetTableValue(cid, 1,0,0,1);
-//		}
-//		break;
-//	case NM_SEL_ADD:
-//		foreach(const int &cid, lstCellId)
-//		{
-//			dsSel->SetTuple1(cid,1);
-//			clrTab->SetTableValue(cid, 1,0,0,1);
-//		}
-//		break;
-//	case NM_SEL_REMOVE:
-//		foreach(const int &cid, lstCellId)
-//		{
-//			dsSel->SetTuple1(cid,0);
-//			clrTab->SetTableValue(cid, 0,0,0,1);
-//		}
-//		break;
-//	case NM_SEL_CLEAR:
-//		for (int r = 0; r < holeAr->GetNumberOfTuples(); ++r)
-//		{
-//			clrTab->SetTableValue(r, 0,0,0,1);
-//			dsSel->SetTuple1(r,0);
-//		}
-//		break;
-//	}
-//
-//	emit dataSetChanged(this);
-//	emit legendChanged(this);
-//	emit visibilityChanged(this);
-//}
-
-
-

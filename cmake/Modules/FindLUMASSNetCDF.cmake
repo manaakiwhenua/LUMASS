f6eb13b3e59c85f7b3999e764161f3551b45b8e4
Alex Herzig <herziga@landcareresearch.co.nz>
2021-05-10 17:50:10 +1200

    fix: FindLUMASSNetCDF.cmake - NetCDF_version vars strictly numeric
    cmake/Modules/FindLUMASSNetCDF.cmake

--- cmake/Modules/FindLUMASSNetCDF.cmake
+++ cmake/Modules/FindLUMASSNetCDF.cmake
@@ -4,10 +4,13 @@
 # this file is part of LUMASS and provided as is
 # - no warranty whatsoever for this being even remotely useful in whichever shape or form!
 #
-execute_process(COMMAND nc-config --version OUTPUT_VARIABLE NETCDF_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND nc-config --version OUTPUT_VARIABLE NETCDF_VERSION_TEXT OUTPUT_STRIP_TRAILING_WHITESPACE)
 execute_process(COMMAND nc-config --includedir OUTPUT_VARIABLE NETCDF_INCLUDE_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
 execute_process(COMMAND nc-config --libdir OUTPUT_VARIABLE NETCDF_LIB_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
 execute_process(COMMAND nc-config --libs OUTPUT_VARIABLE NETCDF_LIBRARIES OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+string(REPLACE "." ";" NETCDF_VERSION_LIST ${NETCDF_VERSION_TEXT})
+list(GET NETCDF_VERSION_LIST 1 NETCDF_VERSION)
 
 if(WIN32)
 	SET(NETCDF_LIBRARY "C:/opt/netcdf-c-4.7.3/liblib/Release/netcdf.lib")
@@ -40,11 +43,14 @@ message(STATUS "NCXX4_CONFIG_PATH: ${NCXX4_CONFIG_PATH}")
 if (NOT NCXX4_CONFIG_PATH)
     message(FATAL_ERROR "Please specify the path to the netcdf-cxx4 config script 'ncxx4-config'")
 else()
-    execute_process(COMMAND ${NCXX4_CONFIG_PATH}/ncxx4-config --version OUTPUT_VARIABLE NCXX4_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND ${NCXX4_CONFIG_PATH}/ncxx4-config --version OUTPUT_VARIABLE NCXX4_VERSION_TEXT OUTPUT_STRIP_TRAILING_WHITESPACE)
     execute_process(COMMAND ${NCXX4_CONFIG_PATH}/ncxx4-config --includedir OUTPUT_VARIABLE NCXX4_INCLUDE_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
     execute_process(COMMAND ${NCXX4_CONFIG_PATH}/ncxx4-config --libdir OUTPUT_VARIABLE NCXX4_LIB_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
     execute_process(COMMAND ${NCXX4_CONFIG_PATH}/ncxx4-config --libs OUTPUT_VARIABLE NCXX4_LIBRARIES OUTPUT_STRIP_TRAILING_WHITESPACE)
 endif()
+
+string(REPLACE "." ";" NCXX4_VERSION_LIST ${NCXX4_VERSION_TEXT})
+list(GET NCXX4_VERSION_LIST 1 NCXX4_VERSION)
 
 if (WIN32)
 
